3bf63dbdc946853a121a26c608ff6aba3991b981
Nguyễn Bá Phước <phcnguyenba@gmail.com>
2024-12-16 18:36:40 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
     origin: '*',
-    credentials: true,
+    credentials: false,
   });
 
   app.useGlobalPipes(
